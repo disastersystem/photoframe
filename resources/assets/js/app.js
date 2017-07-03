9484ee45a212627e9c879c9d1891f5b14e720492
disastersystem <robin.vigdal.bekkevold@gmail.com>
2017-07-03 20:18:26 +0200

    Test.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,7 +2,7 @@ import './bootstrap'
 import router from './router'
 
 import creategroup from './components/dialogs/CreateGroup'
-import photoframe from './components/Frame'
+import photoframe from './components/PhotoFrame'
 
 Vue.component('main-layout', {
 	template: `
